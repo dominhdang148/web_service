f28291239c0b7178c02d382d03b08bed3e48e6ad
dominhdang148 <myclone235@gmail.com>
2023-11-16 13:42:00 +0700

    Update, perhaps switch to nodejs

--- lab03/main.py
+++ lab03/main.py
@@ -1,1 +1,18 @@
-print("Hello world")
+from http.server import *
+import socketserver
+
+
+class myhandler(SimpleHTTPRequestHandler):
+    def do_GET(self):
+        if self.path == '/':
+            return SimpleHTTPRequestHandler.do_GET(self)
+
+
+PORT = 5000
+
+handler = myhandler
+
+
+myserver = socketserver.TCPServer(("", PORT), handler)
+myserver.serve_forever()
+print(f"Server started at {PORT}")
