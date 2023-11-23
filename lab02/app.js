0dfb695d400bf5654a0fb1bc723b89bbb3b7a6e5
dominhdang148 <myclone235@gmail.com>
2023-11-23 08:04:50 +0700

    Update

--- lab02/app.js
+++ lab02/app.js
@@ -4,8 +4,11 @@ const PORT = process.env.PORT || 5000;
 const server = http.createServer(async (req, res) => {
   if (req.url === "/api" && req.method === "GET") {
     res.writeHead(200, { "Content-Type": "application/json" });
-    res.write("Hello there");
-    res.end;
+    res.end(
+      JSON.stringify({
+        message: "Hello world"
+      })
+    );
   } else {
     res.writeHead(404, { "Content-Type": "application/json" });
     res.end(JSON.stringify({ message: "Root not found" }));
