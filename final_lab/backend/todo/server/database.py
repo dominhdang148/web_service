bf2ffa19f1898312dd230fdb0c333ba92459ec6d
dominhdang148 <myclone235@gmail.com>
2023-12-06 01:26:44 +0700

    add helpers

--- final_lab/backend/todo/server/database.py
+++ final_lab/backend/todo/server/database.py
@@ -1,6 +1,18 @@
+from bson.objectid import ObjectId
 import motor.motor_asyncio
 
 MONGO_DATABASE = "mongodb://localhost:27017"
 client = motor.motor_asyncio.AsyncIOMotorClient(MONGO_DATABASE)
 database = client.todo
 todo_collection = database.get_collection("todo_collection")
+
+
+# Helper
+
+
+def todo_helper(todo):
+    return {
+        "id": str(todo["_id"]),
+        "title": todo["title"],
+        "description": todo["description"]
+    }
