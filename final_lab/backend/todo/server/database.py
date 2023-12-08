cfad1e0bf82b9d0208c16d8a390c827ad0780ab7
dominhdang148 <myclone235@gmail.com>
2023-12-08 23:07:14 +0700

    update

--- final_lab/backend/todo/server/database.py
+++ final_lab/backend/todo/server/database.py
@@ -18,8 +18,30 @@ def todo_helper(todo):
     }
 
 
-async def retrieve_todos():
+# Get all todos
+async def get_todos():
     todos = []
     async for todo in todo_collection.find():
         todos.append(todo)
     return todos
+
+
+# Add new todo
+async def add_todo(todo_data: dict):
+    todo = await todo_collection.insert_one(todo_data)
+    new_todo = await todo_collection.find_one({"_id": todo.inserted_id})
+    return todo_helper(new_todo)
+
+
+# Get a todo matched with given ID
+async def get_todo(id: str):
+    todo = await todo_collection.find_one({"_id": ObjectId(id)})
+    if todo:
+        return todo_helper(todo)
+
+
+# Update a todo with a matching ID
+async def update_todo(id: str, data: dict):
+    if len(data) < 1:
+        return False
+    student = await todo_collection.find_one({"_id": ObjectId(id)})
